1e6a753440483abb6dd76fad62bf73f14cc78822
Douglas Diniz Carvalho <douglasdnzcarvalho@gmail.com>
2024-02-20 13:08:38 -0300

    Movendo os arquivos da lib para a pasta dist

--- app/img-2-data.js
+++ app/img-2-data.js
@@ -1,12 +1,17 @@
-export default class Img2Data {
+class Img2Data {
     constructor(id) {
         this._id = id;
+        this._path = './node_modules/@insilicall/img-2-data/index.html';
+    }
+
+    setPath(path) {
+        this._path = path;
     }
 
     load() {
         const div = document.getElementById(this._id);
         const iframe = document.createElement('iframe');
-        iframe.src = 'index.html';
+        iframe.src = this._path;
         iframe.width = '100%';
         iframe.height = '100%';
         div.appendChild(iframe);
--- setup_dist.sh
+++ setup_dist.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir -p dist
 cp app/index.html dist/
 cp app/wpd.min.js dist/
 cp app/img-2-data.js dist/
@@ -9,5 +8,4 @@ cp **/*.ico dist/
 cp **/*.png dist/
 cp -r app/images dist/
 cp -r app/node_modules dist/
-cp package.json dist/
 cp .github/README.md dist/
